a3bccad8a820cda54d89ae3ff33ee1c1b21e27cb
CS-HH <1042331526@qq.com>
2018-04-07 15:50:49 +0800

    使用pagehelper+thymeleaf进行分页查询完整实现

--- springbootpagehelper/src/main/java/com/iFox/hh/rest/CountryController.java
+++ springbootpagehelper/src/main/java/com/iFox/hh/rest/CountryController.java
@@ -5,6 +5,7 @@ import com.iFox.hh.service.CountryService;
 import com.iFox.hh.model.Country;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
