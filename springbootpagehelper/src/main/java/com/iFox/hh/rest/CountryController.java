c66bb7480c0fd7a90a595aff3829a5cfd468b594
CS-HH <1042331526@qq.com>
2018-04-09 22:03:07 +0800

    直接访问html页面

--- springbootpagehelper/src/main/java/com/iFox/hh/rest/CountryController.java
+++ springbootpagehelper/src/main/java/com/iFox/hh/rest/CountryController.java
@@ -8,9 +8,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author exphuhong
@@ -31,8 +34,30 @@ public class CountryController extends BaseController {
         ModelAndView modelAndView = new ModelAndView("country");
         modelAndView.addObject("page", pageInfo);
         if (country.getKeyWords() != null && !country.getKeyWords().equals("")) {
+            logger.info("关键字查询,关键字是："+country.getKeyWords());
             modelAndView.addObject("keyWords", country.getKeyWords());
         }
         return modelAndView;
+    }
+
+
+    /**
+     * 返回json数据给easyuiy页面
+     *@author exphuhong
+     *@since  21:31 2018/4/9
+     *
+     */
+
+    @RequestMapping("/getAllToEasyUi")
+    public @ResponseBody
+    Map<String,Object> getCountry(Country country) {
+        List<Country> countryList = countryService.getAll(country);
+        PageInfo<Country> pageInfo = new PageInfo<>(countryList);
+
+        Map<String, Object> data = new HashMap<>();
+
+        data.put("pages", pageInfo.getPages());
+        data.put("columns", pageInfo.getList());
+        return data;
     }
 }
